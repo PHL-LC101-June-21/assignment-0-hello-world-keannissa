231ef5486696a4bee4fb3ef78785d77c49f04531
keannissa <64294460+keannissa@users.noreply.github.com>
2021-06-10 18:32:27 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-function hello(world!) {
+function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
 	return "Hello World!";
